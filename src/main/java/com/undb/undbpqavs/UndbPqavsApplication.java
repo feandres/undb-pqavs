a2ec83cbd82ebba52de73f2a1a3686700e560cc0
Felipe Andres <felipevsconcelos@gmail.com>
2023-10-19 14:22:06 -0300

    Initial commit

---
+++ src/main/java/com/undb/undbpqavs/UndbPqavsApplication.java
@@ -0,0 +1,13 @@
+package com.undb.undbpqavs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndbPqavsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndbPqavsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undb/undbpqavs/UndbPqavsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undb.undbpqavs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndbPqavsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
